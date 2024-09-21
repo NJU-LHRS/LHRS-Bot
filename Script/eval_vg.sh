4dbb25d8508cb4b8fe4dbdba867e37628a2edfab
pUmpKin-Co <1059005704@qq.com>
2024-09-21 15:00:13 +0800

    update script

--- Script/eval_vg.sh
+++ Script/eval_vg.sh
@@ -1,15 +1,12 @@
-SCRIPT_PATH=/home/aiscuser/LHRS-Bot/main_vg.py
-OUTPUT_PATH=/home/aiscuser/Output/VGEval/RSVG_DIOR
-CONFI_PATH=/home/aiscuser/LHRS-Bot/Config/multi_modal_eval.yaml
-MODEL_PATH=/mnt/default/projects/v-dmuhtar/data/LHRS/Stage3/checkpoints/FINAL.pt
-DATA_PATH=/home/aiscuser/Datastore/InstructDataset/RSVG_DIOR_Image
-DATA_TARGET=/home/aiscuser/Datastore/Eval/VGEvalDataset/RSVG_DIOR_test.json
-
-source /opt/conda/bin/activate lhrs
+SCRIPT_PATH=../main_vg.py
+OUTPUT_PATH="" # Output path
+CONFI_PATH=../Config/multi_modal_eval.yaml
+MODEL_PATH=""  # Model path
+DATA_PATH=""   # Data path
+DATA_TARGET="" # Data target path
 
 deepspeed \
     --master_port=29500 \
-    --include="node-0:$1" \
     $SCRIPT_PATH \
     -c $CONFI_PATH \
     --batch-size 1 \
